56374e2aacc95c8ed0686089213db71ce36c2ec5
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2018-02-20 08:15:38 +0100

    Added validator and tests in nunit

---
+++ src/NipValidator/NipValidator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace Rychusoft.Validators
+{
+    public class NipValidator
+    {
+        private static int[] weights = { 6, 5, 7, 2, 3, 4, 5, 6, 7, 0 };
+
+        public static bool IsValid(string nip)
+        {
+            if (IsEmptyOrNotProperLength(nip))
+                return false;
+
+            if (!IsNumber(nip))
+                return false;
+
+            if (!IsChecksumValid(nip))
+                return false;
+
+            if (AreAllDigitsTheSame(nip))
+                return false;
+
+            return true;
+        }
+
+        private static bool IsEmptyOrNotProperLength(string nip)
+        {
+            return string.IsNullOrWhiteSpace(nip) || nip.Length != 10;
+        }
+
+        private static bool IsNumber(string nip)
+        {
+            return nip.All(Char.IsDigit);
+        }
+
+        private static bool IsChecksumValid(string nip)
+        {
+            int checksum = GenerateChecksum(nip);
+
+            return checksum.ToString() == nip[9].ToString();
+        }
+
+        private static int GenerateChecksum(string nip)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                sum += weights[i] * int.Parse(nip[i].ToString());
+            }
+
+            return sum % 11;
+        }
+
+        private static bool AreAllDigitsTheSame(string nip)
+        {
+            return nip.Distinct().Count() == 1;
+        }
+    }
+}
---
+++ tests/NipValidatorTests/NipValidatorTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace NipValidatorTests
+{
+    [TestClass()]
+    public class NipValidatorTests
+    {
+        [DataTestMethod]
+        [DataRow("6275273253")]
+        [DataRow("3943797120")]
+        [DataRow("8784066227")]
+        [DataRow("7094711247")]
+        [DataRow("8674068337")]
+        [DataRow("7140018423")]
+        [DataRow("5147857226")]
+        [DataRow("8442209706")]
+        [DataRow("6539354354")]
+        [DataRow("5207812599")]
+        public void IsValid_ValidNipsTest(string nip)
+        {
+            Assert.IsTrue(NipValidator.IsValid(nip));
+        }
+
+        [DataTestMethod]
+        [DataRow("627527325")]
+        [DataRow("39437971")]
+        [DataRow("8784066")]
+        [DataRow("709471")]
+        [DataRow("86740")]
+        [DataRow("7140")]
+        [DataRow("514")]
+        [DataRow("84")]
+        [DataRow("6")]
+        public void IsValid_TooShortNipsTest(string nip)
+        {
+            Assert.IsFalse(NipValidator.IsValid(nip));
+        }
+
+        [DataTestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        public void IsValid_EmptyNipsTest(string nip)
+        {
+            Assert.IsFalse(NipValidator.IsValid(nip));
+        }
+
+        [DataTestMethod]
+        [DataRow("62752732531")]
+        [DataRow("394379712012")]
+        [DataRow("8784066227133")]
+        [DataRow("70947112471456")]
+        public void IsValid_TooLongNipsTest(string nip)
+        {
+            Assert.IsFalse(NipValidator.IsValid(nip));
+        }
+
+        [DataTestMethod]
+        [DataRow("          ")]
+        [DataRow("         0")]
+        [DataRow("0         ")]
+        [DataRow("      0   ")]
+        [DataRow(";ierhf;adr")]
+        [DataRow("0000000000")]
+        [DataRow("1111111111")]
+        [DataRow("1010101010")]
+        [DataRow("0101010101")]
+        [DataRow("aaaaaaaaaa")]
+        [DataRow(";lizsedujr")]
+        [DataRow("9999999999")]
+        public void IsValid_MonkeyInvalidNipsTest(string nip)
+        {
+            Assert.IsFalse(NipValidator.IsValid(nip));
+        }
+    }
+}
