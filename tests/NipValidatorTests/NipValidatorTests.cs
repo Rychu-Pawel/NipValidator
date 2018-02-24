711618276ed209dde0e06111d6265b014efd57a5
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2018-02-24 15:17:01 +0100

    Set xUnit up and converted tests to XUnit

--- tests/NipValidatorTests/NipValidatorTests.cs
+++ tests/NipValidatorTests/NipValidatorTests.cs
@@ -1,75 +1,76 @@
-﻿using System;
+﻿using Rychusoft.Validators;
+using System;
+using Xunit;
 
 namespace NipValidatorTests
 {
-    [TestClass()]
     public class NipValidatorTests
     {
-        [DataTestMethod]
-        [DataRow("6275273253")]
-        [DataRow("3943797120")]
-        [DataRow("8784066227")]
-        [DataRow("7094711247")]
-        [DataRow("8674068337")]
-        [DataRow("7140018423")]
-        [DataRow("5147857226")]
-        [DataRow("8442209706")]
-        [DataRow("6539354354")]
-        [DataRow("5207812599")]
+        [Theory]
+        [InlineData("6275273253")]
+        [InlineData("3943797120")]
+        [InlineData("8784066227")]
+        [InlineData("7094711247")]
+        [InlineData("8674068337")]
+        [InlineData("7140018423")]
+        [InlineData("5147857226")]
+        [InlineData("8442209706")]
+        [InlineData("6539354354")]
+        [InlineData("5207812599")]
         public void IsValid_ValidNipsTest(string nip)
         {
-            Assert.IsTrue(NipValidator.IsValid(nip));
+            Assert.True(NipValidator.IsValid(nip));
         }
 
-        [DataTestMethod]
-        [DataRow("627527325")]
-        [DataRow("39437971")]
-        [DataRow("8784066")]
-        [DataRow("709471")]
-        [DataRow("86740")]
-        [DataRow("7140")]
-        [DataRow("514")]
-        [DataRow("84")]
-        [DataRow("6")]
+        [Theory]
+        [InlineData("627527325")]
+        [InlineData("39437971")]
+        [InlineData("8784066")]
+        [InlineData("709471")]
+        [InlineData("86740")]
+        [InlineData("7140")]
+        [InlineData("514")]
+        [InlineData("84")]
+        [InlineData("6")]
         public void IsValid_TooShortNipsTest(string nip)
         {
-            Assert.IsFalse(NipValidator.IsValid(nip));
+            Assert.False(NipValidator.IsValid(nip));
         }
 
-        [DataTestMethod]
-        [DataRow(null)]
-        [DataRow("")]
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
         public void IsValid_EmptyNipsTest(string nip)
         {
-            Assert.IsFalse(NipValidator.IsValid(nip));
+            Assert.False(NipValidator.IsValid(nip));
         }
 
-        [DataTestMethod]
-        [DataRow("62752732531")]
-        [DataRow("394379712012")]
-        [DataRow("8784066227133")]
-        [DataRow("70947112471456")]
+        [Theory]
+        [InlineData("62752732531")]
+        [InlineData("394379712012")]
+        [InlineData("8784066227133")]
+        [InlineData("70947112471456")]
         public void IsValid_TooLongNipsTest(string nip)
         {
-            Assert.IsFalse(NipValidator.IsValid(nip));
+            Assert.False(NipValidator.IsValid(nip));
         }
 
-        [DataTestMethod]
-        [DataRow("          ")]
-        [DataRow("         0")]
-        [DataRow("0         ")]
-        [DataRow("      0   ")]
-        [DataRow(";ierhf;adr")]
-        [DataRow("0000000000")]
-        [DataRow("1111111111")]
-        [DataRow("1010101010")]
-        [DataRow("0101010101")]
-        [DataRow("aaaaaaaaaa")]
-        [DataRow(";lizsedujr")]
-        [DataRow("9999999999")]
+        [Theory]
+        [InlineData("          ")]
+        [InlineData("         0")]
+        [InlineData("0         ")]
+        [InlineData("      0   ")]
+        [InlineData(";ierhf;adr")]
+        [InlineData("0000000000")]
+        [InlineData("1111111111")]
+        [InlineData("1010101010")]
+        [InlineData("0101010101")]
+        [InlineData("aaaaaaaaaa")]
+        [InlineData(";lizsedujr")]
+        [InlineData("9999999999")]
         public void IsValid_MonkeyInvalidNipsTest(string nip)
         {
-            Assert.IsFalse(NipValidator.IsValid(nip));
+            Assert.False(NipValidator.IsValid(nip));
         }
     }
 }
